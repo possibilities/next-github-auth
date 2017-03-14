b53f921770de73bc9c6e66bc0dece9299ee2c259
Mike Bannister <notimpossiblemike@gmail.com>
2017-03-13 21:04:06 -0700

    Meaningless cleanup

--- decorators/PrivatePage.js
+++ decorators/PrivatePage.js
@@ -1,7 +1,8 @@
 import PublicPage from '../decorators/PublicPage'
 import EnsureSignedIn from '../decorators/EnsureSignedIn'
+import InjectGithubUser from '../decorators/InjectGithubUser'
 import compose from '../modules/compose'
 
-const PrivatePage = compose(PublicPage, EnsureSignedIn)
+const PrivatePage = compose(PublicPage, InjectGithubUser, EnsureSignedIn)
 
 export default PrivatePage
--- decorators/PublicPage.js
+++ decorators/PublicPage.js
@@ -1,8 +1,9 @@
 import compose from '../modules/compose'
 
 import InjectEnvVars from '../decorators/InjectEnvVars'
-import InjectGithubAccessToken from '../decorators/InjectGithubAccessToken'
 import InjectGithubUser from '../decorators/InjectGithubUser'
+import InjectGithubAccessToken
+  from '../decorators/InjectGithubAccessToken'
 
 const injectGithubClientId = InjectEnvVars({
   GITHUB_CLIENT_ID: 'githubClientId'
