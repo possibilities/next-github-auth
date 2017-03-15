6d37867ded02585efafe0f03aa044c92d8b25152
Mike Bannister <notimpossiblemike@gmail.com>
2017-03-15 00:30:48 -0700

    Move SignOutPage to component

---
+++ src/components/SignOutPage.js
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import getGithubAccessTokenCookie from '../../modules/getGithubAccessTokenCookie'
+
+export default class SignOutPage extends Component {
+  static getInitialProps ({ req, res }) {
+    if (!process.browser) {
+      const githubAccessTokenCookie = getGithubAccessTokenCookie(req, '')
+      res.writeHead(302, {
+        'Set-Cookie': githubAccessTokenCookie,
+        Location: '/'
+      })
+      res.end()
+    }
+
+    return {}
+  }
+
+  constructor () {
+    super()
+    window.location = '/sign-out'
+  }
+
+  render () { return null }
+}
