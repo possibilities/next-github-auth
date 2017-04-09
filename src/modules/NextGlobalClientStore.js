df71897f2b35b1af0abbf373984cb831cd0aa2bb
Mike Bannister <notimpossiblemike@gmail.com>
2017-04-08 18:14:15 -0700

    Cleanup `NextGlobalClientStore`

--- src/modules/NextGlobalClientStore.js
+++ src/modules/NextGlobalClientStore.js
@@ -1,19 +1,12 @@
-if (process.browser) {
-  window.___nextJsData || (window.___nextJsData = {})
-}
-
 // In next it's useful to have a global spot to share data. This is used
 // throughout to enable sharing server generated values across client loaded
 // pages.
 
-const NextGlobalClientStore = {
-  get (key) {
-    return window.___nextJsData[key]
-  },
+const data = {}
 
-  set (key, val) {
-    window.___nextJsData[key] = val
-  }
+const NextGlobalClientStore = {
+  get (key) { return data[key] },
+  set (key, val) { data[key] = val }
 }
 
 export default NextGlobalClientStore
